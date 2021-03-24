f732698f481df875d367267e6fccd436de415e5f
rortegaa <rortegaa@everis.com>
2021-03-24 17:54:35 +0100

    Versión V1 GIT-Dual

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,20 @@
+package com.everis.git;
+
+/**
+ * Formación Dual - IES Alixar
+ * 
+ * @author rortegaa
+ *
+ */
+public class AppMain {
+
+	/**
+	 * Método Principal de la clase.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("¡Hola Mundo!");
+	}
+
+}
